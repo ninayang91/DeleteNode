472292d42277ff29db593a4349691743567534d2
ninayang91 <na.yang@hotmail.com>
2016-09-03 22:58:12 -0400

    Add codes

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Hashtable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Node a=new Node(1);
+		Node b=new Node(2,a);
+		Node c=new Node(3,b);
+		Node head=new Node(1,c);
+		//System.out.println(kToLast(head, 5));
+	}
+
+
+}
